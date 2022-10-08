13157f4a551df135db3e5ad7789adb7008c12d1c
Benedict <benedictryan80@gmail.com>
2022-10-08 15:35:08 +0700

    update font family

--- components/MoviesOrTv.js
+++ components/MoviesOrTv.js
@@ -9,7 +9,7 @@ function MoviesOrTv({ title, filmList }) {
 					{title === 'movie' ? 'Popular Movie' : 'Popular TV'}
 				</h1>
 				{filmList?.results?.slice(15, 20).map((film, idx) => (
-					<h2 className="p-3" key={idx}>
+					<h2 className="p-3 font-display" key={idx}>
 						{film.original_title || film.original_name}
 					</h2>
 				))}
