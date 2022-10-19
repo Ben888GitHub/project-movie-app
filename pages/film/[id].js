3fa28ea2d21f77a6e64c9c6c79a9aac27d919ce1
Benedict <benedictryan80@gmail.com>
2022-10-19 14:49:08 +0700

    implement responsive titles

--- pages/film/[id].js
+++ pages/film/[id].js
@@ -10,18 +10,24 @@ function Movie({ id }) {
 				<title>{id}</title>
 				<meta name="description" content="Movie name" />
 			</Head>
-			<div className="container mx-auto">
-				<div className="md:flex lg:flex md:flex-wrap lg:flex-wrap justify-between px-4 md:px-6 py-2.5 mt-5 ">
+			<div className="container mx-auto lg:max-w-6xl md:max-w-6xl mt-3 lg:mt-5 md:mt-5">
+				<div className="flex flex-wrap justify-between px-4 md:px-6">
 					<div>
-						<p className="text-4xl  font-medium">Cobra Kai</p>
-						<p className="text-2xl font-display mt-2">
+						<p className="text-2xl md:text-4xl lg:text-4xl  font-medium">
+							{id}
+						</p>
+						<p className="text-lg md:text-2xl lg:text-2xl font-display mt-0 md:mt-2 lg:mt-2">
 							TV Series · 2018- · 30m{' '}
 						</p>
 					</div>
 
 					<div>
-						<p className="text-2xl font-display">TMDB Rating </p>
-						<p className="text-3xl font-display mt-2">9/10 </p>
+						<p className="text-lg md:text-2xl lg:text-2xl  font-display">
+							TMDB Rating{' '}
+						</p>
+						<p className="text-2xl md:text-3xl lg:text-3xl font-display mt-0 md:mt-2 lg:mt-2">
+							9/10{' '}
+						</p>
 					</div>
 				</div>
 			</div>
