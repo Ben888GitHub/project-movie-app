bb0698873191a9aba4c3cb27eb85e8a4142c96e9
Benedict <benedictryan80@gmail.com>
2022-10-29 18:59:41 +0700

    fix the background color declaration

--- components/movie/Genres.js
+++ components/movie/Genres.js
@@ -1,18 +1,14 @@
-import dynamic from 'next/dynamic';
 import { colors } from '../../utils/badgeColors';
 
 function Genres({ genres }) {
 	console.log(genres);
-
-	// console.log(getBadgeColors(genres.name));
-	// console.log(colors['Action']);
 
 	return (
 		<div className="flex">
 			{genres.map((genre, idx) => (
 				<div key={idx}>
 					<span
-						className={`text-white  ${
+						className={`text-white  bg-${
 							colors[genre.name]
 						} text-sm font-medium mr-2 px-2.5 py-0.5 rounded focus:outline-none focus:ring-4`}
 					>
@@ -25,8 +21,3 @@ function Genres({ genres }) {
 }
 
 export default Genres;
-{
-	/* <p className="mr-5" key={idx}>
-					{genre.name}
-				</p> */
-}
