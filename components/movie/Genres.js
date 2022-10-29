e9c9b0afd7f05a771be29389fe52a419226e1344
Benedict <benedictryan80@gmail.com>
2022-10-29 19:03:12 +0700

    try with hardcode color

--- components/movie/Genres.js
+++ components/movie/Genres.js
@@ -8,9 +8,7 @@ function Genres({ genres }) {
 			{genres.map((genre, idx) => (
 				<div key={idx}>
 					<span
-						className={`text-white  bg-${
-							colors[genre.name]
-						} text-sm font-medium mr-2 px-2.5 py-0.5 rounded focus:outline-none focus:ring-4`}
+						className={`text-white  bg-amber-700 text-sm font-medium mr-2 px-2.5 py-0.5 rounded focus:outline-none focus:ring-4`}
 					>
 						{genre.name}
 					</span>
