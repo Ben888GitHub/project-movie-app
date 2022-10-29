185e8ded856511140b43b4bc9fb7504ecea42dea
Benedict <benedictryan80@gmail.com>
2022-10-29 19:10:30 +0700

    put the colors data in the component 2

--- components/movie/Genres.js
+++ components/movie/Genres.js
@@ -1,29 +1,29 @@
 // import { colors } from '../../utils/badgeColors';
-const colors = {
-	Action: 'red-700',
-	Comedy: 'blue-700',
-	Thriller: 'gray-700',
-	Crime: 'purple-700',
-	Adventure: 'green-700',
-	Animation: 'orange-700',
-	Drama: 'amber-700',
-	Documentary: 'stale-700',
-	Family: 'sky-700',
-	Fantasy: 'emerald-700',
-	History: 'yellow-700',
-	Horror: 'black',
-	Music: 'cyan-700',
-	Romance: 'rose-700',
-	Soap: 'lime-700',
-	Reality: 'zinc-700',
-	'Sci-Fi & Fantasy': 'emerald-700',
-	'Action & Adventure': 'red-700',
-	War: 'pink-700',
-	Western: 'teal-700'
-};
-function Genres({ genres }) {
-	console.log(genres);
 
+function Genres({ genres }) {
+	// console.log(genres);
+	const colors = {
+		Action: 'red-700',
+		Comedy: 'blue-700',
+		Thriller: 'gray-700',
+		Crime: 'purple-700',
+		Adventure: 'green-700',
+		Animation: 'orange-700',
+		Drama: 'amber-700',
+		Documentary: 'stale-700',
+		Family: 'sky-700',
+		Fantasy: 'emerald-700',
+		History: 'yellow-700',
+		Horror: 'black',
+		Music: 'cyan-700',
+		Romance: 'rose-700',
+		Soap: 'lime-700',
+		Reality: 'zinc-700',
+		'Sci-Fi & Fantasy': 'emerald-700',
+		'Action & Adventure': 'red-700',
+		War: 'pink-700',
+		Western: 'teal-700'
+	};
 	return (
 		<div className="flex">
 			{genres.map((genre, idx) => (
