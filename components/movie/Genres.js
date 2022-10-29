0e92dcb462501f387939f3fc7329e9e9f208b5c3
Benedict <benedictryan80@gmail.com>
2022-10-29 19:27:29 +0700

    put the colors data in the component 3

--- components/movie/Genres.js
+++ components/movie/Genres.js
@@ -1,8 +1,31 @@
 import { useState, useEffect } from 'react';
-import { colors } from '../../utils/badgeColors';
+// import { colors } from '../../utils/badgeColors';
 
 function Genres({ genres }) {
 	// console.log(genres);
+	const colors = {
+		Action: 'red-700',
+		Comedy: 'blue-700',
+		Thriller: 'gray-700',
+		Crime: 'purple-700',
+		Adventure: 'green-700',
+		Animation: 'orange-700',
+		Drama: 'amber-700',
+		Documentary: 'stale-700',
+		Family: 'sky-700',
+		Fantasy: 'emerald-700',
+		History: 'yellow-700',
+		Horror: 'black',
+		Music: 'cyan-700',
+		Romance: 'rose-700',
+		Soap: 'lime-700',
+		Reality: 'zinc-700',
+		'Sci-Fi & Fantasy': 'emerald-700',
+		'Action & Adventure': 'red-700',
+		War: 'pink-700',
+		Western: 'teal-700'
+	};
+
 	const [genreColors, setGenreColors] = useState({});
 	useEffect(() => {
 		setGenreColors(colors);
@@ -12,13 +35,13 @@ function Genres({ genres }) {
 		<div className="flex">
 			{genres.map((genre, idx) => (
 				<div key={idx}>
-					<p
+					<span
 						className={`text-white  bg-${
 							genreColors[genre.name]
 						} text-sm font-medium mr-2 px-2.5 py-0.5 rounded focus:outline-none focus:ring-4`}
 					>
 						{genre.name}
-					</p>
+					</span>
 				</div>
 			))}
 		</div>
