2aaf4f06d778c660d2ff72894b27e176f4f38bf4
Benedict <benedictryan80@gmail.com>
2022-10-29 21:15:21 +0700

    switch to react style property

--- utils/badgeColors.js
+++ utils/badgeColors.js
@@ -21,4 +21,27 @@ const colors = {
 	Western: 'teal-700'
 };
 
-export { colors };
+const badgeStyleColors = {
+	Action: '#b91c1c',
+	Comedy: '#1d4ed8',
+	Thriller: '#374151',
+	Crime: '#7e22ce',
+	Adventure: '#15803d',
+	Animation: '#c2410c',
+	Drama: '#b45309',
+	Documentary: '#334155',
+	Family: '#0369a1',
+	Fantasy: '#047857',
+	History: '#a16207',
+	Horror: '#000000',
+	Music: '#0e7490',
+	Romance: '#be123c',
+	Soap: '#4d7c0f',
+	Reality: '#3f3f46',
+	'Sci-Fi & Fantasy': '#047857',
+	'Action & Adventure': '#b91c1c',
+	War: '#be185d',
+	Western: '#0f766e'
+};
+
+export { colors, badgeStyleColors };
