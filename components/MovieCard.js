9c785994e8418c952b8df4334b2a7ebb76796ae8
Benedict <benedictryan80@gmail.com>
2022-11-14 14:44:17 +0700

    add as property in the Link to fetch Movie or TV

--- components/MovieCard.js
+++ components/MovieCard.js
@@ -14,9 +14,9 @@ function MovieCard({
 	return (
 		<div className="inline-flex m-3 md:m-5 lg:m-5">
 			<div className="max-w-[165px] md:max-w-xs lg:max-w-xs bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700  ">
-				<Link href={`/${tvOrMovie}/${id}`} passHref>
+				<Link href={`/${tvOrMovie}/${id}`} as={`/${tvOrMovie}/${id}`} passHref>
 					<div>
-						{/* <Image
+						<Image
 							src={`https://image.tmdb.org/t/p/w500${poster}`}
 							alt={title || name}
 							width={500}
@@ -27,8 +27,7 @@ function MovieCard({
 							loading="lazy"
 							placeholder="empty"
 							className="rounded-t-lg cursor-pointer relative"
-						/> */}
-						<h1>Hey</h1>
+						/>
 					</div>
 				</Link>
 
