bb208ce52b7e1315bf0b32aa91e6f362a79f1db7
Benedict <benedictryan80@gmail.com>
2022-11-14 14:49:28 +0700

    replace router history

--- components/MovieCard.js
+++ components/MovieCard.js
@@ -14,7 +14,12 @@ function MovieCard({
 	return (
 		<div className="inline-flex m-3 md:m-5 lg:m-5">
 			<div className="max-w-[165px] md:max-w-xs lg:max-w-xs bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700  ">
-				<Link href={`/${tvOrMovie}/${id}`} shallow passHref>
+				<Link
+					href={`/${tvOrMovie}/${id}`}
+					shallow={true}
+					replace={true}
+					passHref
+				>
 					<div>
 						<Image
 							src={`https://image.tmdb.org/t/p/w500${poster}`}
