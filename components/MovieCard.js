01cf75050c95913895c54bd20b14bab98a86103d
Benedict <benedictryan80@gmail.com>
2022-11-15 02:20:38 +0700

    bring back Link

--- components/MovieCard.js
+++ components/MovieCard.js
@@ -19,26 +19,26 @@ function MovieCard({
 		// use inline-flex in div, if no Carousel
 		<div className="m-3 md:m-5 lg:m-5">
 			<div className="mx-auto mb-10  max-w-[165px] md:max-w-xs lg:max-w-xs bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700  ">
-				{/* <Link href={`/${tvOrMovie}/${id}`} passHref> */}
-				<div
-					onClick={() => router.push(`/${tvOrMovie}/${id}`)}
-					className="rounded-t-lg cursor-pointer bg-white dark:bg-gray-800 border border-gray-200 dark:border-gray-700"
-				>
-					<Image
-						quality={50}
-						priority
-						height={750}
-						width={500}
-						src={`https://image.tmdb.org/t/p/w500${poster}`}
-						alt={poster}
-						// todo, remove the quality and priority if there's any issue
+				<Link href={`/${tvOrMovie}/${id}`} passHref>
+					<div
+						// onClick={() => router.push(`/${tvOrMovie}/${id}`)}
+						className="rounded-t-lg cursor-pointer bg-white dark:bg-gray-800 border border-gray-200 dark:border-gray-700"
+					>
+						<Image
+							quality={50}
+							priority
+							height={750}
+							width={500}
+							src={`https://image.tmdb.org/t/p/w500${poster}`}
+							alt={poster}
+							// todo, remove the quality and priority if there's any issue
 
-						// loading="lazy"
-						// placeholder="empty"
-						className="rounded-t-lg cursor-pointer relative"
-					/>
-				</div>
-				{/* </Link> */}
+							// loading="lazy"
+							// placeholder="empty"
+							className="rounded-t-lg cursor-pointer relative"
+						/>
+					</div>
+				</Link>
 				<div className="p-5 lg:mt-0 md:mt-0 -mt-2 text-left">
 					<p className="mb-2 sm:text-md md:text-xl lg:text-xl font-bold tracking-tight text-gray-900 dark:text-white">
 						{title || name}
