60cc30deb7dfcf8ca97591fd5521fe3eaa5eac0b
Benedict <benedictryan80@gmail.com>
2022-11-14 14:40:31 +0700

    try changing it to non image

--- components/MovieCard.js
+++ components/MovieCard.js
@@ -14,24 +14,24 @@ function MovieCard({
 	return (
 		<div className="inline-flex m-3 md:m-5 lg:m-5">
 			<div className="max-w-[165px] md:max-w-xs lg:max-w-xs bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700  ">
-				<div>
-					<Link href={`/${tvOrMovie}/${id}`} passHref>
-						<a>
-							<Image
-								src={`https://image.tmdb.org/t/p/w500${poster}`}
-								alt={title || name}
-								width={500}
-								height={750}
-								// todo, remove the quality and priority if there's any issue
-								// priority
-								quality={50}
-								// loading="lazy"
-								placeholder="empty"
-								className="rounded-t-lg cursor-pointer relative"
-							/>
-						</a>
-					</Link>
-				</div>
+				<Link href={`/${tvOrMovie}/${id}`} passHref>
+					<div>
+						{/* <Image
+							src={`https://image.tmdb.org/t/p/w500${poster}`}
+							alt={title || name}
+							width={500}
+							height={750}
+							// todo, remove the quality and priority if there's any issue
+							// priority
+							quality={50}
+							loading="lazy"
+							placeholder="empty"
+							className="rounded-t-lg cursor-pointer relative"
+						/> */}
+						<h1>Hey</h1>
+					</div>
+				</Link>
+
 				<div className="p-5 lg:mt-0 md:mt-0 -mt-2 text-left">
 					<p className="mb-2 sm:text-md md:text-xl lg:text-xl font-bold tracking-tight text-gray-900 dark:text-white">
 						{title || name}
