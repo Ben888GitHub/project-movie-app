b5f4fd260aced6e1ee399138701875e5e96aee48
Benedict <benedictryan80@gmail.com>
2022-11-15 02:47:36 +0700

    optimize code

--- components/MovieCard.js
+++ components/MovieCard.js
@@ -14,15 +14,15 @@ function MovieCard({
 	movie_date,
 	id
 }) {
-	const router = useRouter();
+	// const router = useRouter();
 	return (
 		// use inline-flex in div, if no Carousel
 		<div className="m-3 md:m-5 lg:m-5">
-			<div className="mx-auto mb-10  max-w-[165px] md:max-w-xs lg:max-w-xs bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700  ">
+			<div className="mx-auto   max-w-[165px] md:max-w-xs lg:max-w-xs bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700  ">
 				<Link href={`/${tvOrMovie}/${id}`} passHref>
 					<div
 						// onClick={() => router.push(`/${tvOrMovie}/${id}`)}
-						className="rounded-t-lg cursor-pointer bg-white dark:bg-gray-800 border border-gray-200 dark:border-gray-700"
+						className="rounded-t-lg cursor-pointer bg-white dark:bg-gray-800"
 					>
 						<Image
 							quality={50}
