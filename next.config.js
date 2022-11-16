9014d53e86e52ace2bf45e5eca2261e056196105
Benedict <benedictryan80@gmail.com>
2022-11-16 17:34:54 +0700

    try other way to implement security headers

--- next.config.js
+++ next.config.js
@@ -1,53 +1,20 @@
-// const headers = require('./headers');
-const ContentSecurityPolicy = `
-  frame-src 'none';
-  frame-ancestors 'none';
-`;
+const headers = require('./headers');
 
 const nextConfig = {
-	reactStrictMode: true,
+	reactStrictMode: false,
 	swcMinify: true,
 	async headers() {
 		return [
 			{
 				source: '/(.*)',
-				headers: [
-					{
-						key: 'X-DNS-Prefetch-Control',
-						value: 'on'
-					},
-					{
-						key: 'X-Content-Type-Options',
-						value: 'nosniff'
-					},
-					{
-						key: 'X-Frame-Options',
-						value: 'sameorigin'
-					},
-					{
-						key: 'X-XSS-Protection',
-						value: '1; mode=block'
-					},
-					{
-						key: 'Referrer-Policy',
-						value: 'same-origin'
-					},
-					{
-						key: 'Permissions-Policy',
-						value: 'geolocation=*' // allow specified policies here
-					},
-					{
-						key: 'Content-Security-Policy',
-						value: ContentSecurityPolicy.replace(/\s{2,}/g, ' ').trim()
-					}
-				]
+				headers
 			}
 		];
 	},
-	i18n: {
-		locales: ['en'],
-		defaultLocale: 'en'
-	},
+	// i18n: {
+	// 	locales: ['en'],
+	// 	defaultLocale: 'en'
+	// },
 	env: {
 		API_KEY: process.env.API_KEY
 	},
