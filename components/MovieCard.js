ad9975e41f34b1fbd901fda4622725f627c3b219
Benedict <benedictryan80@gmail.com>
2022-11-16 22:23:01 +0700

    set scroll from the top in film page 3

--- components/MovieCard.js
+++ components/MovieCard.js
@@ -19,7 +19,7 @@ function MovieCard({
 		// use inline-flex in div, if no Carousel
 		<div className="m-3 md:m-5 lg:m-5">
 			<div className="mx-auto mb-10  max-w-[165px] md:max-w-xs lg:max-w-xs bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700  ">
-				<Link href={`/${tvOrMovie}/${id}`} passHref scroll={false}>
+				<Link href={`/${tvOrMovie}/${id}`} passHref>
 					<div
 						// onClick={() => router.push(`/${tvOrMovie}/${id}`)}
 						className="rounded-t-lg cursor-pointer bg-white dark:bg-gray-800"
