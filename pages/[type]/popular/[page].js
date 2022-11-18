ebef6e712ed15cc14ba2eb4dfce93774173a5a09
Benedict <benedictryan80@gmail.com>
2022-11-18 16:56:27 +0700

    implement pagination function to next page

--- pages/[type]/popular/[page].js
+++ pages/[type]/popular/[page].js
@@ -57,25 +57,38 @@ function Popular({ type, page }) {
 						</div>
 					</Link>
 				</button>
-				<a
-					href="#"
+				<button
+					disabled={Number(page) === 40}
 					className="inline-flex items-center px-4 py-2 text-sm font-medium text-gray-500 bg-white border border-gray-300 rounded-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
 				>
-					Next
-					<svg
-						aria-hidden="true"
-						className="w-5 h-5 ml-2"
-						fill="currentColor"
-						viewBox="0 0 20 20"
-						xmlns="http://www.w3.org/2000/svg"
+					<Link
+						href={{
+							pathname: `/[type]/popular/[page]`,
+							query: {
+								type,
+								page: Number(page) === 40 ? 40 : Number(page) + 1
+							}
+						}}
+						passHref
 					>
-						<path
-							fillRule="evenodd"
-							d="M12.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-2.293-2.293a1 1 0 010-1.414z"
-							clipRule="evenodd"
-						></path>
-					</svg>
-				</a>
+						<div className="flex">
+							Next
+							<svg
+								aria-hidden="true"
+								className="w-5 h-5 ml-2"
+								fill="currentColor"
+								viewBox="0 0 20 20"
+								xmlns="http://www.w3.org/2000/svg"
+							>
+								<path
+									fillRule="evenodd"
+									d="M12.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-2.293-2.293a1 1 0 010-1.414z"
+									clipRule="evenodd"
+								/>
+							</svg>
+						</div>
+					</Link>
+				</button>
 			</div>
 			<br />
 			<div className="mx-auto container text-center">
