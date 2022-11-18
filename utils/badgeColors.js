7e604591f0248e5d4afe7dce21aa8b9ac069b262
Benedict <benedictryan80@gmail.com>
2022-11-18 17:16:00 +0700

    add new genre 2

--- utils/badgeColors.js
+++ utils/badgeColors.js
@@ -1,27 +1,3 @@
-const colors = {
-	Action: 'red-700',
-	Comedy: 'blue-700',
-	Thriller: 'gray-700',
-	Crime: 'purple-700',
-	Adventure: 'green-700',
-	Animation: 'orange-700',
-	Drama: 'amber-700',
-	Documentary: 'stale-700',
-	Family: 'sky-700',
-	Fantasy: 'emerald-700',
-	History: 'yellow-700',
-	Horror: 'black',
-	Music: 'cyan-700',
-	Romance: 'rose-700',
-	Soap: 'lime-700',
-	Reality: 'zinc-700',
-	'Sci-Fi & Fantasy': 'emerald-700',
-	'Action & Adventure': 'red-700',
-	War: 'pink-700',
-	Western: 'teal-700',
-	'Science Fiction': 'amber-800'
-};
-
 const badgeStyleColors = {
 	Action: '#b91c1c',
 	Comedy: '#1d4ed8',
