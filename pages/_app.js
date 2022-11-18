67dae4b57c1f813a09ec4d7b722f4e89889cdfcc
Benedict <benedictryan80@gmail.com>
2022-11-18 14:29:51 +0700

    Set up React Query Provider and Hydration

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,35 @@
 import { ThemeProvider } from 'next-themes';
 import Layout from '../components/Layout';
 import '../styles/globals.css';
+import {
+	QueryClient,
+	QueryClientProvider,
+	Hydrate
+} from '@tanstack/react-query';
+import { useState } from 'react';
 
 function MyApp({ Component, pageProps }) {
+	const [queryClient] = useState(
+		() =>
+			new QueryClient({
+				defaultOptions: {
+					queries: {
+						refetchOnWindowFocus: false,
+						refetchOnMount: false
+					}
+				}
+			})
+	);
+
 	return (
 		<ThemeProvider defaultTheme="system" attribute="class">
-			<Layout>
-				<Component {...pageProps} />
-			</Layout>
+			<QueryClientProvider client={queryClient}>
+				<Hydrate state={pageProps.dehydratedState}>
+					<Layout>
+						<Component {...pageProps} />
+					</Layout>
+				</Hydrate>
+			</QueryClientProvider>
 		</ThemeProvider>
 	);
 }
