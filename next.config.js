9c62871ca3b4353fe6e4dac669cdd575f52bb67b
Benedict <benedictryan80@gmail.com>
2022-11-30 14:55:09 +0700

    optimize tailwindcss

--- next.config.js
+++ next.config.js
@@ -20,14 +20,6 @@ const nextConfig = {
 		minimumCacheTTL: 31536000,
 		formats: ['image/webp']
 	}
-	// experimental: {
-	// 	scrollRestoration: true
-	// }
-	// todo, if i18n is enabled then security headers are not scanned online, why?
-	// i18n: {
-	// locales: ['en'],
-	// defaultLocale: 'en'
-	// }
 };
 
 module.exports = nextConfig;
--- tailwind.config.js
+++ tailwind.config.js
@@ -14,31 +14,6 @@ module.exports = {
 			semibold: ['Poppins'],
 			'extra-bold': ['"Poppins"'],
 			black: ['Poppins']
-		},
-
-		extend: {
-			// colors: {
-			// 	Action: '#b91c1c',
-			// 	Comedy: '#1d4ed8',
-			// 	Thriller: '#374151',
-			// 	Crime: '#7e22ce',
-			// 	Adventure: '#15803d',
-			// 	Animation: '#c2410c',
-			// 	Drama: '#b45309',
-			// 	Documentary: '#334155',
-			// 	Family: '#0369a1',
-			// 	Fantasy: '#047857',
-			// 	History: '#a16207',
-			// 	Horror: '#000000',
-			// 	Music: '#0e7490',
-			// 	Romance: '#be123c',
-			// 	Soap: '#4d7c0f',
-			// 	Reality: '#3f3f46',
-			// 	'Sci-Fi & Fantasy': '#047857',
-			// 	'Action & Adventure': '#b91c1c',
-			// 	War: '#be185d',
-			// 	Western: '#0f766e'
-			// }
 		}
 	},
 	plugins: []
