6f437c22686917987abeb3c7bb1a63a738d945b2
Benedict <benedictryan80@gmail.com>
2022-12-02 12:20:05 +0700

    implement Modal to search tv shows or movies 5

--- components/SearchFilm.js
+++ components/SearchFilm.js
@@ -77,8 +77,8 @@ function SearchFilm({ open, setOpen }) {
 				>
 					<div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
 				</Transition.Child>
-				<div className="fixed inset-0 z-10 overflow-y-auto ">
-					{/* <div className=" flex h-[200px] md:h-[270px] lg:h-[270px] items-end justify-center p-4 text-center sm:items-center sm:p-0"> */}
+				{/* <div className="fixed inset-0 z-10 overflow-y-auto "> */}
+				<div className=" flex h-[200px] md:h-[270px] lg:h-[270px] items-end justify-center p-4 text-center sm:items-center sm:p-0">
 					<div className=" flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
 						<Transition.Child
 							as={Fragment}
