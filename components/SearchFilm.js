ef186af765d09df2b00075df55699e452cfd490b
Benedict <benedictryan80@gmail.com>
2022-12-02 14:52:35 +0700

    implement Modal to search tv shows or movies 7

--- components/SearchFilm.js
+++ components/SearchFilm.js
@@ -14,7 +14,7 @@ const people = [
 
 function SearchFilm({ open, setOpen }) {
 	const [queryFilm, setQueryFilm] = useState('');
-	const [selected, setSelected] = useState(people[0]);
+	const [selected, setSelected] = useState('');
 	const [query, setQuery] = useState('');
 	const cancelButtonRef = useRef(null);
 
@@ -48,6 +48,21 @@ function SearchFilm({ open, setOpen }) {
 	// 		</Combobox.Option>
 	// 	));
 
+	// data && console.log(data);
+
+	// todo, optimize this code
+	const filteredFilm = data?.results.filter(
+		(film) =>
+			film.name
+				?.toLowerCase()
+				.replace(/\s+/g, '')
+				.includes(queryFilm?.toLowerCase().replace(/\s+/g, '')) ||
+			film.original_title
+				?.toLowerCase()
+				.replace(/\s+/g, '')
+				.includes(queryFilm?.toLowerCase().replace(/\s+/g, ''))
+	);
+
 	const filteredPeople =
 		query === ''
 			? people
@@ -63,7 +78,6 @@ function SearchFilm({ open, setOpen }) {
 			<Dialog
 				as="div"
 				className=" relative z-10"
-				// initialFocus={cancelButtonRef}
 				onClose={() => setOpen(false)}
 			>
 				<Transition.Child
@@ -78,8 +92,8 @@ function SearchFilm({ open, setOpen }) {
 					<div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
 				</Transition.Child>
 				<div className="fixed inset-0 z-10 overflow-y-auto ">
-					<div className=" flex h-[200px] md:h-[270px] lg:h-[270px] items-end justify-center p-4 text-center sm:items-center sm:p-0">
-						{/* <div className=" flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0"> */}
+					{/* <div className=" flex h-[200px] md:h-[270px] lg:h-[270px] items-end justify-center p-4 text-center sm:items-center sm:p-0"> */}
+					<div className=" flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
 						<Transition.Child
 							as={Fragment}
 							enter="ease-out duration-300"
@@ -91,51 +105,25 @@ function SearchFilm({ open, setOpen }) {
 						>
 							{/* This is to adjust the Dialog size and shape */}
 							<Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-md">
-								{/* <div className="bg-white dark:bg-[#2D3748] px-4 pt-5 pb-4 sm:p-6 sm:pb-4"> */}
-								{/* <div className=" sm:items-start"> */}
-								{/* <div className="mt-3 sm:mt-0 sm:text-left">
-											<Dialog.Title
-												as="h3"
-												className=" text-lg font-medium leading-6 text-gray-900 dark:text-white mb-4"
-											>
-												Search TV Shows or Movies
-											</Dialog.Title>
-										</div> */}
+								{/* <Combobox value={selected} onChange={setSelected}> */}
 								<Combobox value={selected} onChange={setSelected}>
 									<Combobox.Input
 										placeholder="Search TV Shows or Movies"
 										className="font-medium bg-gray-50  text-gray-900 text-md rounded-t-md  w-full p-2.5 dark:bg-gray-600  dark:placeholder-gray-400 dark:text-white"
-										// displayValue={(person) => person.name}
-										onChange={(event) => setQuery(event.target.value)}
+										// value={queryFilm}
+										onChange={(event) => setQueryFilm(event.target.value)}
 									/>
-									{/* <Combobox.Button className="absolute inset-y-0 right-0 flex items-center pr-2">
-										<svg
-											xmlns="http://www.w3.org/2000/svg"
-											fill="none"
-											viewBox="0 0 24 24"
-											strokeWidth={2.5}
-											stroke="currentColor"
-											className="w-6 h-6"
-										>
-											<path
-												strokeLinecap="round"
-												strokeLinejoin="round"
-												d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"
-											/>
-										</svg>
-									</Combobox.Button> */}
 									<Transition
-										// as={Fragment}
 										leave="transition ease-in duration-100"
 										leaveFrom="opacity-100"
 										leaveTo="opacity-0"
-										afterLeave={() => setQuery('')}
+										afterLeave={() => setQueryFilm('')}
 									>
-										{query && (
+										{queryFilm && (
 											<Combobox.Options className="dark:bg-[#1a202c] max-h-60 w-full overflow-auto rounded-b-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
-												{filteredPeople.map((person) => (
+												{filteredFilm?.slice(0, 7).map((film, idx) => (
 													<Combobox.Option
-														key={person.id}
+														key={idx}
 														className={({ active }) =>
 															`font-display relative cursor-pointer select-none py-2 pl-10 pr-4 ${
 																active
@@ -143,25 +131,16 @@ function SearchFilm({ open, setOpen }) {
 																	: 'text-gray-900  dark:text-white'
 															}`
 														}
-														value={person}
+														value={film.name || film.original_title}
+														onClick={() => console.log(film)}
 													>
-														{person.name}
+														{film.name || film.original_title}
 													</Combobox.Option>
 												))}
 											</Combobox.Options>
 										)}
 									</Transition>
 								</Combobox>
-								{/* <button
-									ref={cancelButtonRef}
-									type="button"
-									className="inline-flex justify-center px-4 py-2 text-sm font-medium text-blue-900 bg-blue-100 border border-transparent rounded-md hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-blue-500"
-									onClick={() => setOpen(false)}
-								>
-									Cancel
-								</button> */}
-								{/* </div> */}
-								{/* </div> */}
 							</Dialog.Panel>
 						</Transition.Child>
 					</div>
