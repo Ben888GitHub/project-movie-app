2f34157c62cf2af151dae8ab6b648dd38f522254
Benedict <benedictryan80@gmail.com>
2022-12-02 18:15:26 +0700

    optimize api calls 2

--- search_film_api.js
+++ search_film_api.js
@@ -17,10 +17,6 @@ const fetchSearchFilm = async (queryFilm) => {
 
 	const movies = await axios(options).then((res) => res.data);
 
-	// const movies = await fetch(
-	//     `https://api.themoviedb.org/3/search/multi?api_key=456ec94d71f9702ddcbbc1166b40f922&language=en-US&query=${queryFilm}`
-	// );
-	// const moviesJson = await movies.json();
 	return movies;
 };
 
